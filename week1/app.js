9ea1aed7c3b5902d53bbdcca4ff664ff0e69d45a
whereisrmsqhs <whereisrmsqhs@gmail.com>
2022-04-21 22:08:26 +0900

    js_week1

---
+++ week1/app.js
@@ -0,0 +1,31 @@
+const number = 10;
+console.log(number);
+
+let num = 10;
+console.log(num);
+
+let array = [];
+let obj = {};
+let func = function(){};
+
+console.log(typeof([]));
+
+let integer = 10;
+let IsInfinity = 10/0;
+let IsNaN = 10 / "칠";
+
+console.log(IsInfinity, IsNaN);
+
+let sum = 0.1 + 0.2;
+console.log(sum);
+
+let str = "\"abc";
+let str2 = 'def';
+
+console.log(str, str2);
+
+let num1;           
+console.log(num1);
+
+let num2 = null;    //의도적으로 비워놨다.
+console.log(num2);
