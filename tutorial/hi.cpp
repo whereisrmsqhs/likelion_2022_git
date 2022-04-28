8aa4a8ea3c1d2ba24b44e1af62a2f7c99082ec29
whereisrmsqhs <whereisrmsqhs@gmail.com>
2022-04-29 03:34:13 +0900

    Python basic

--- tutorial/hi.cpp
+++ tutorial/hi.cpp
@@ -1,9 +1,20 @@
-#include<iostream>
+#include <iostream>
+#include <sstream>
+#include <string>
 
-int b = 1;
-int c = 2;
-auto lambda = [](int a) {return a;}
+int main() {
+  // greet the user
+  std::string name;
+  std::cout << "What is your name? ";
+  std::getline(std::cin, name);
+  std::cout << "Hello " << name << ", nice to meet you.\n";
 
-int main(){
-
+  // stringstream 은 마치 문자열을 입력 스트림으로 생각하게 해줍니다.
+  std::istringstream input;
+  input.str("1\n2\n3\n4\n5\n6\n7\n");
+  int sum = 0;
+  for (std::string line; std::getline(input, line);) {
+    sum += std::stoi(line);
+  }
+  std::cout << "\nThe sum is: " << sum << "\n";
 }
